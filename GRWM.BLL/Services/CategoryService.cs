e069eb7f8e02b9dab529c570659023c6b627d129
muhamedhamedvl <mh1191128@gmail.com>
2025-09-14 16:17:30 +0300

    feat(category): add service and interface (CategoryService, ICategoryService)

---
+++ GRWM.BLL/Services/CategoryService.cs
@@ -0,0 +1,54 @@
+﻿using GRWM.BLL.Services.IServices;
+using GRWMJobs.DAL.Models;
+using GRWMJobs.DAL.Repositores.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GRWM.BLL.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly IGenericRepository<Category> _categoryRepo;
+
+        public CategoryService(IGenericRepository<Category> categoryRepo)
+        {
+            _categoryRepo = categoryRepo;
+        }
+
+        public async Task<IEnumerable<Category>> GetCategoriesByTrackAsync(int trackId)
+        {
+            var all = await _categoryRepo.GetAllAsync();
+            return all.Where(c => c.TrackId == trackId);
+        }
+
+        public async Task<Category?> GetCategoryByIdAsync(int id)
+        {
+            return await _categoryRepo.GetByIdAsync(id);
+        }
+
+        public async Task AddCategoryAsync(Category category)
+        {
+            await _categoryRepo.AddAsync(category);
+            await _categoryRepo.SaveAsync();
+        }
+
+        public async Task UpdateCategoryAsync(Category category)
+        {
+            _categoryRepo.Update(category);
+            await _categoryRepo.SaveAsync();
+        }
+
+        public async Task DeleteCategoryAsync(int id)
+        {
+            var category = await _categoryRepo.GetByIdAsync(id);
+            if (category != null)
+            {
+                _categoryRepo.Delete(category);
+                await _categoryRepo.SaveAsync();
+            }
+        }
+    }
+}
+
+
---
+++ GRWM.BLL/Services/IServices/ICategoryService.cs
@@ -0,0 +1,17 @@
+﻿using GRWMJobs.DAL.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GRWM.BLL.Services.IServices
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<Category>> GetCategoriesByTrackAsync(int trackId);
+        Task<Category?> GetCategoryByIdAsync(int id);
+        Task AddCategoryAsync(Category category);
+        Task UpdateCategoryAsync(Category category);
+        Task DeleteCategoryAsync(int id);
+    }
+}
+
+
