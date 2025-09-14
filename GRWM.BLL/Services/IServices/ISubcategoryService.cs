c02601f6befcfdb5ee9649ea0d4a16129a353b82
muhamedhamedvl <mh1191128@gmail.com>
2025-09-14 16:20:24 +0300

    feat(subcategory): add service and interface (SubCategoryService, ISubCategoryService)

---
+++ GRWM.BLL/Services/IServices/ISubcategoryService.cs
@@ -0,0 +1,17 @@
+﻿using GRWMJobs.DAL.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GRWM.BLL.Services.IServices
+{
+    public interface ISubcategoryService
+    {
+        Task<IEnumerable<Subcategory>> GetSubcategoriesByCategoryAsync(int categoryId);
+        Task<Subcategory?> GetSubcategoryByIdAsync(int id);
+        Task AddSubcategoryAsync(Subcategory subcategory);
+        Task UpdateSubcategoryAsync(Subcategory subcategory);
+        Task DeleteSubcategoryAsync(int id);
+    }
+}
+
+
